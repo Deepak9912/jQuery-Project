0875b08ca68a2298218b2b0a5db1ff8cd92800f4
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-02-02 21:12:20 +0000

    add axios movie request

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-    
-})
+    $('#searchForm').on('submit', function(e){
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com?s='+searchText)
+        .then(function(response){
+            console.log(response);
+            let movies = response.data.Search;
+            let output = "";
+            $.each(movies, (index, function(movie){
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imbdID}')" class="btn btn-primary" href="#">Movie Details</a>
+                        </div>
+                    </div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                `
+            }))
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+}
