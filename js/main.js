4df95d2334e2a977c7be33af2290be5d6e6be1ba
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-02-03 20:58:26 +0000

    create weather app

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
-    $('#searchForm').on('submit', function(e){
-        let searchText = $('#searchText').val();
-        getMovies(searchText);
-        e.preventDefault();
-    });
+    $('#form-sub').on('submit', function(e){
+        let city = $('#city').val();
+        getCity(city);
+    })
 });
 
-function getMovies(searchText){
-    axios.get('http://www.omdbapi.com?s='+searchText)
-        .then(function(response){
-            console.log(response);
-            let movies = response.data.Search;
-            let output = "";
-            $.each(movies, (index, function(movie){
-                output += `
-                    <div class="col-md-3">
-                        <div class="well text-center">
-                            <img src="${movie.Poster}">
-                            <h5>${movie.Title}</h5>
-                            <a onclick="movieSelected('${movie.imbdID}')" class="btn btn-primary" href="#">Movie Details</a>
-                        </div>
-                    </div>
-                    <div class="col-md-3"></div>
-                    <div class="col-md-3"></div>
-                `
-            }))
-        })
-        .catch(function(err){
-            console.log(err);
-        })
+function getCity(city){
+    var request;
+    city.preventDefault();
+
+    request = $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        type: "GET",
+        data: {
+            q: $("#city").val(),
+            appid: '128b2a70fd3d8d83854ae6d95ec1a1eb',
+            units: 'metric'
+        }
+    });
+
+    request.done(function(response){
+        formatSearch(response);
+    });
+}
+
+function formatSearch(JsonObject){
+    var city_name = JsonObject.name;
+    var city_weather = JsonObject.weather[0].main;
+    var city_temp = JsonObject.main.temp;
+
+    $('#city-name').text(city_name);
+    $('#city-weather').text(city_weather);
+    $('#city-temp').text(city_temp+ "Celsius");
 }
