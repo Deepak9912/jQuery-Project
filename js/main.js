ce0742cb92a47cd7e4d418eb06810b8404037ed3
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-02-08 21:23:39 +0000

    add weather api

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
 $(document).ready(function(){
-    $('#form-sub').on('submit', function(e){
-        let city = $('#city').val();
-        getCity(city);
+    $('#form-submit').submit('click', function(e){
+        performSearch(e);
     })
-});
+})
 
-function getCity(city){
+function performSearch(e){
     var request;
-    city.preventDefault();
+    e.preventDefault();
 
     request = $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/weather',
         type: "GET",
         data: {
-            q: $("#city").val(),
-            appid: '128b2a70fd3d8d83854ae6d95ec1a1eb',
+            q: $('#city').val(),
+            appid: '5192abbc047ff7b1faaf66409ebeb16e',
             units: 'metric'
         }
     });
-
     request.done(function(response){
         formatSearch(response);
-    });
-}
+    })
 
-function formatSearch(JsonObject){
-    var city_name = JsonObject.name;
-    var city_weather = JsonObject.weather[0].main;
-    var city_temp = JsonObject.main.temp;
+    function formatSearch(jsonObject){
+        var city_name = jsonObject.name;
+        var city_weather = jsonObject.weather[0].main;
+        var feels_like = jsonObject.main.feels_like;
+        var city_temp = jsonObject.main.temp;
+        var city_description = jsonObject.weather[0].description;
+        var city_humidity = jsonObject.humidity;
+        var city_visibility = jsonObject.visibility;
+        var city_country = jsonObject.sys.country;
+        var city_timezone = jsonObject.timezone;
 
-    $('#city-name').text(city_name);
-    $('#city-weather').text(city_weather);
-    $('#city-temp').text(city_temp+ "Celsius");
+        $('#city-name').text(city_name);
+        $('#city_weather').text(city_weather);
+        $('#city_description').text(city_description);
+        $('#feels_like').text(feels_like+ " C feels like");
+        $('#city-temp').text(city_temp+ " Celsius");
+        $('#city_humidity').text(city_humidity+ " humidity");
+        $('#city_visibility').text(city_visibility+ " Visibility");
+        $('#city_country').text(city_country);
+        $('#city_timezone').text(city_timezone);
+    }
 }
